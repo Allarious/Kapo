032efac8cffab9a4bfbe0d970050684b8750d5aa
Amin <maisaai1997@gmail.com>
2018-08-14 19:02:37 +0430

    testing with chert code

---
+++ accounts/urls.py
@@ -0,0 +1,35 @@
+from django.conf.urls import url
+from apps.accounts import views
+
+app_name = "accounts"
+urlpatterns = [
+    url(r'^signup/$', views.SignupView.as_view(), name='signup'),
+    url(r'^login/$', views.LoginView.as_view(), name='login'),
+    url(r'^logout/$', views.LogoutView.as_view(), name='logout'),
+    url(r'^update_profile/$', views.UpdateProfileView.as_view(), name='update_profile'),
+    url(r'^create_team/(?P<challenge_id>[0-9]+)$', views.create_team, name='create_team'),
+    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        views.activate, name='activate'),
+    url(r'^create_team/success_create_team$', views.success_create_team, name='success_create_team'),
+    url(r'^panel/(?P<participation_id>[0-9]+)?$', views.team_management, name='panel'),
+    url(r'^panel/accept_participation/(?P<participation_id>[0-9]+)$', views.accept_participation,
+        name='accept_participation'),
+    url(r'^panel/reject_participation/(?P<participation_id>[0-9]+)$', views.reject_participation,
+        name='reject_participation'),
+    url(r'^panel/cancel_participation/(?P<participation_id>[0-9]+)/(?P<user_id>[0-9]+)$',
+        views.cancel_participation_request, name='cancel_participation_request'),
+    url(r'^panel/add_participation/(?P<participation_id>[0-9]+)$', views.add_participation,
+        name='add_participation'),
+    url(r'^panel/set_final_submission/(?P<submission_id>[0-9]+)', views.set_final_submission,
+        name='set final submission'),
+    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        views.activate, name='activate'),
+    url(r'^email_sent$', views.email_sent, name='email_sent'),
+    url(r'^email_confirm$', views.email_confirm, name='email_confirm'),
+    url(r'^email_invalid$', views.email_invalid, name='email_invalid'),
+    url(r'^challenge_a_team/(?P<participation_id>\d+)$', views.challenge_a_team, name='challenge_a_team'),
+
+    url(r'^panel/submissions', views.submissions, name='panel_submissions'),
+    url(r'^panel/team$', views.team_management, name='panel_team_management'),
+    url(r'^panel/bhistory', views.battle_history, name='panel_battle_history'),
+    url(r'^panel/teampc/(?P<team_pc>\d+)$', views.change_team_pc, name='panel_change_team_pc')
---
+++ forms/forms.py
@@ -0,0 +1,108 @@
+from django.conf import settings
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.sites.shortcuts import get_current_site
+from django.core.mail import send_mail
+from django.core.validators import RegexValidator
+from django.forms.models import ModelForm
+from django.template.loader import render_to_string
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
+from django.utils.translation import ugettext_lazy as _
+from apps.accounts.models import Profile
+from apps.accounts.tokens import account_activation_token
+from captcha.fields import ReCaptchaField
+
+
+class SignUpForm(UserCreationForm):
+    first_name = forms.CharField(max_length=30, required=True)
+    last_name = forms.CharField(max_length=30, required=True)
+    email = forms.EmailField(max_length=254, required=True)
+    organization = forms.CharField(max_length=255, required=True)
+    phone_regex = RegexValidator(regex=r'^\d{8,15}$',
+                                 message=_("Please enter your phone number correctly!"))
+    phone_number = forms.CharField(validators=[phone_regex], required=False)
+    age = forms.IntegerField(required=False)
+    captcha = ReCaptchaField()
+
+    def is_valid(self):
+        if not super().is_valid():
+            return False
+        if not settings.ENABLE_REGISTRATION:
+            self.add_error(None, _('Registration is closed. See you next year.'))
+            return False
+        return True
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.is_active = False
+        if commit:
+            user.save()
+            domain = get_current_site(self.request)
+            email_text = render_to_string('email/acc_active.txt', {
+                'user': user,
+                'domain': domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': account_activation_token.make_token(user)
+            })
+            email_html = render_to_string('email/acc_active.html', {
+                'user': user,
+                'domain': domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': account_activation_token.make_token(user)
+            })
+
+            profile = Profile(
+                user=user,
+                phone_number=self.cleaned_data['phone_number'],
+                organization=self.cleaned_data['organization'],
+                age=self.cleaned_data['age']
+            )
+            profile.save()
+
+            send_mail(subject='Activate Your Account',
+                      message=email_text,
+                      from_email='Sharif AI Challenge <info@aichallenge.ir>',
+                      recipient_list=[user.email],
+                      fail_silently=False,
+                      html_message=email_html
+                      )
+
+        return user
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'organization','age', 'phone_number', 'email', 'password1', 'password2')
+
+
+class UpdateProfileForm(ModelForm):
+    first_name = forms.CharField(max_length=30, required=True, help_text='Optional.')
+    last_name = forms.CharField(max_length=30, required=True, help_text='Optional.')
+    email = forms.EmailField(max_length=254, required=True, help_text='Required. Inform a valid email address.')
+    age = forms.IntegerField()
+    password1 = forms.CharField(required=False, widget=forms.PasswordInput)
+    password2 = forms.CharField(required=False, widget=forms.PasswordInput)
+
+    def clean(self):
+        super().clean()
+        password1 = self.cleaned_data['password1']
+        password2 = self.cleaned_data['password2']
+        if password1 and password2 and password1 != password2:
+            self.add_error('password1', 'password error')
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        profile = user.profile
+        profile.phone_number = self.cleaned_data['phone_number']
+        profile.age = self.cleaned_data['age']
+        profile.organization = self.cleaned_data['organization']
+
+        if commit:
+            user.save()
+            profile.save()
+        return profile
+
+    class Meta:
+        model = Profile
+        fields = ('first_name', 'last_name', 'email','phone_number','age', 'password1', 'password2')
