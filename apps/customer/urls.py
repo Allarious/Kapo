c16a68ae8e0752de5560cfae3705c55d6fbb441a
Amin <maisaai1997@gmail.com>
2018-08-17 21:05:07 +0430

    customer urls included

---
+++ apps/customer/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls import url
+
+from apps.accounts.views import login_user
+from . import views
+
+app_name = "customer"
--- kapo/urls.py
+++ kapo/urls.py
@@ -2,8 +2,9 @@
 from . import views
 from django.contrib import admin
 from django.conf.urls import include, url
-
+from apps.customer import urls as customer_urls
 
 urlpatterns = [
+    url(r'^', include(customer_urls)),
 
 ]
