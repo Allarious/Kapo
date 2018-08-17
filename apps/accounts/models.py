c1dc9a6a10625db1aeb836a2e6ffeaa78184ea18
Amin <maisaai1997@gmail.com>
2018-08-17 22:15:33 +0430

    customer model in customer

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -13,21 +13,7 @@ class MyUser(AbstractUser):
     is_employee = models.BooleanField(default=False)
 
 
-SEX_CHOICES = (('male', 'Male'), ('female', 'Female'), ('other', 'Other'), ('blank', 'blank'))
-
-
-class Customer(models.Model):
-    user = models.OneToOneField(MyUser, on_delete=models.CASCADE, primary_key=True)
-    firstName = models.CharField(max_length=20)
-    lastName = models.CharField(max_length=20)
-    gender = models.CharField(max_length=10, choices=SEX_CHOICES, default='Male')
-    day = models.IntegerField(default=0)
-    month = models.IntegerField(default=0)
-    year = models.IntegerField(default=0)
-    nationalId = models.CharField(max_length=10)
-    city = models.CharField(max_length=10, default='blank')
-    address = models.CharField(max_length=100, default='st')
-    phone = models.CharField(max_length=12)
+SEX_CHOICES = (('male', 'Male'), ('female', 'Female'), ('other', 'Other'), ('blank', 'Blank'))
 
 
 class Manager(models.Model):
--- apps/customer/models.py
+++ apps/customer/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+from apps.accounts.models import MyUser, SEX_CHOICES
+
+
+class Customer(models.Model):
+    user = models.OneToOneField(MyUser, on_delete=models.CASCADE, primary_key=True)
+    firstName = models.CharField(max_length=20)
+    lastName = models.CharField(max_length=20)
+    gender = models.CharField(max_length=10, choices=SEX_CHOICES, default='Male')
+    day = models.IntegerField(default=0)
+    month = models.IntegerField(default=0)
+    year = models.IntegerField(default=0)
+    nationalId = models.CharField(max_length=10)
+    city = models.CharField(max_length=10, default='blank')
+    address = models.CharField(max_length=100, default='st')
+    phone = models.CharField(max_length=12)
