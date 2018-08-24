88a92904428c1a4739af1f6a13707b28bf38011c
awesdea <maisaai1997@gmail.com>
2018-08-24 23:39:16 +0430

    app fee transaction view added

--- apps/transactions/views.py
+++ apps/transactions/views.py
@@ -27,10 +27,9 @@ def exam_transactions_view(request):
         form = ExamTransactionForm(request.POST)
         if form.is_valid():
             exam = form.save(commit=False)
-            exam.dollar_cost = form.cleaned_data['dollar_cost']
             exam.owner = customer
             cost = exam.dollar_cost * exam_wage
-            if customer.dollar_wallet < exam.dollar_cost:
+            if customer.dollar_wallet < cost:
                 form.add_error('dollar_cost',
                                'You need {} more dollars!'.format(cost - customer.dollar_wallet))
                 return render(request, 'exam_transactions.html',
@@ -59,28 +58,55 @@ def app_fee_transactions_view(request):
     euro_rate = int(Configuration.objects.get(key='euro').value)
 
     if request.method == 'POST':
-        form = ExamTransactionForm(request.POST)
+        form = ApplicationTuitionFeeTransactionForm(request.POST)
         if form.is_valid():
-            exam = form.save(commit=False)
-            exam.dollar_cost = form.cleaned_data['dollar_cost']
-            exam.owner = customer
+            fee = form.save(commit=False)
+            fee.owner = customer
+            if fee.dollar_cost > fee.euro_cost and fee.euro_cost == 0:
+                cost = fee.dollar_cost * fee_wage
 
-            if customer.dollar_wallet < exam.dollar_cost:
-                form.add_error('dollar_cost',
-                               'You need {} more dollars!'.format(exam.dollar_cost - customer.dollar_wallet))
-                return render(request, 'exam_transactions.html',
+                if customer.dollar_wallet < cost:
+                    form.add_error('dollar_cost',
+                                   'You need {} more Dollars!'.format(cost - customer.dollar_wallet))
+                    return render(request, 'fee_transactions.html',
+                                  {'customer': customer, 'form': form, 'fee_wage': fee_wage,
+                                   'dollar_rate': dollar_rate,
+                                   'euro_rate': euro_rate})
+                fee.save()
+                customer.dollar_wallet -= fee.dollar_cost
+                customer.save()
+                fee.paid = True
+                fee.save()
+
+
+            elif fee.euro_cost > fee.dollar_cost and fee.dollar_cost == 0:
+                cost = fee.dollar_cost * fee_wage
+
+                if customer.euro_wallet < cost:
+                    form.add_error('euro_cost',
+                                   'You need {} more Euros!'.format(cost - customer.euro_wallet))
+                    return render(request, 'fee_transactions.html',
+                                  {'customer': customer, 'form': form, 'fee_wage': fee_wage,
+                                   'dollar_rate': dollar_rate,
+                                   'euro_rate': euro_rate})
+
+                fee.save()
+                customer.euro_wallet -= fee.euro_cost
+                customer.save()
+                fee.paid = True
+                fee.save()
+            else:
+                form.add_error('dollar_cost','sth went wrong!')
+                return render(request, 'fee_transactions.html',
                               {'customer': customer, 'form': form, 'fee_wage': fee_wage,
                                'dollar_rate': dollar_rate,
                                'euro_rate': euro_rate})
-            exam.save()
-            customer.dollar_wallet -= exam.dollar_cost
-            customer.save()
-            exam.paid = True
-            exam.save()
+            # TODO add money to manager
+
             return redirect('/customer/')
     else:
-        form = ExamTransactionForm()
+        form = ApplicationTuitionFeeTransactionForm()
 
-    return render(request, 'exam_transactions.html',
-                  {'customer': customer, 'form': form, 'exam_wage': exam_wage, 'dollar_rate': dollar_rate,
+    return render(request, 'fee_transactions.html',
+                  {'customer': customer, 'form': form, 'fee_wage': fee_wage, 'dollar_rate': dollar_rate,
                    'euro_rate': euro_rate})
